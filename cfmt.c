f8f179b165fdb17a859bca40d412330d1472711d
Jon Craton <jncraton@gmail.com>
2023-01-18 09:57:51 -0500

    Complete file opening and closing in handout code

--- cfmt.c
+++ cfmt.c
@@ -3,5 +3,34 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-int main(int argc, char ** argv) {
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    /*
+     * Terminate if not given filename argument
+     */
+    exit(1);
+  }
+
+  /*
+   * Load input file into buf. Note that file size is limited to 2^16 bytes/
+   * Additional data will be truncated
+   */
+  char buf[65535];
+  int src = open(argv[1], O_RDONLY);
+  read(src, buf, 65535);
+  close(src);
+
+  /*
+   * Open output file for writing
+   */
+  int dst = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC);
+
+  /*
+   * TODO: Write formatted file to output
+   */
+
+  /*
+   * Close output file
+   */
+  close(dst);
 }
